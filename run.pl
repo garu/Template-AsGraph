bd58ca7b8a54eef10e8c4a74c0579022fe8a53b3
Breno G. de Oliveira <garu@cpan.org>
2009-07-11 14:28:28 -0300

    removed unnecessary files from last commit

--- run.pl
+++
@@ -1,9 +0,0 @@
-use lib 'lib';
-use Template::AsGraph;
-
-my $graph = Template::AsGraph->graph('1.tt');
-
-if (open(my $png, '|-', 'dot -Tpng -o routes.png')) {
-	print $png $graph->as_graphviz;
-	close($png);
-}
